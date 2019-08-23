1d0cb6757d9733bd90f80422c12d44a4500c106d
Simuzax <simuzax@gmail.com>
2019-08-23 11:57:20 -0300

    190823

---
+++ JogoPuzzle/Assets/Scripts/Platform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    float timeValue;
+
+    bool isRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.AttachTimer(timeValue, switchDirection, isLooped: true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    void move()
+    {
+        if (isRight) transform.Translate(Vector3.right * speed * Time.deltaTime);
+        else transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+    void switchDirection()
+    {
+        isRight = !isRight;
+    }
+}
